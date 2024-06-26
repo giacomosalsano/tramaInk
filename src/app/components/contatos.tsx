70f11cbb09c12c9fef492fe6283548d324f731f1
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-06-26 00:59:34 -0300

    started to style the contacts

--- src/app/components/contatos.tsx
+++ src/app/components/contatos.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export function Contatos () {
   return (
     <div className="grid auto-row-max mt-10 max-w-auto">
@@ -5,11 +7,67 @@ export function Contatos () {
           Contatos{" "}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
           </span>
-        </h2>
+      </h2>
         <p className="font-Bungee font-medium text-text-secondary">
           Nossos contatos e redes sociais:
         </p>
+        <span>
+          <div>
+                <div>
+                  <div className="inline-flex p-3">
+                    <Image 
+                      className="rounded-full"
+                      src="/tramalogo.png"
+                      alt="logoTrama"
+                      width={50}
+                      height={50}/>
+                    <strong className="m-3 text-center">Trama Ink Studio</strong>
+                  </div>
+                  <div className="inline-flex p-3">
+                    <Image 
+                      className="rounded-full"
+                      src="/vicprofilepic.jpg"
+                      alt="vic foto" 
+                      height={50}
+                      width={50}/>
+                    <strong className="m-3 text-center">Victória Santana</strong>
+                  </div>
+                  <div>
+                  <div className="inline-flex p-3">
+                    <Image 
+                      className="rounded-full"
+                      src="/scavazini.jpg"
+                      alt=""
+                      height={50}
+                      width={50}/>
+                    <strong className="m-3 text-center">Lucas Scavazini</strong>
+                  </div>
+                  <div className="inline-flex p-3">
+                    <Image 
+                      className="rounded-full"
+                      src="/gui.jpg"
+                      alt=""
+                      height={50}
+                      width={50}/>
+                    <strong className="m-3 text-center">Gui Bethlem</strong>
+                  </div>
+                  <div className="inline-flex p-3">
+                    <Image 
+                      className="rounded-full"
+                      src="/melso.jpg"
+                      alt=""
+                      height={50}
+                      width={50}/>
+                    <strong className="m-3 text-center">Melso Melsão</strong>
+                  </div>
+                </div>
+                </div>
+            </div>
+                
+              
+          </span>  
     </div>
+          
 
   )
 }
