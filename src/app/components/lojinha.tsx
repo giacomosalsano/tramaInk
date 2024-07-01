a0a83dadd12098b52279def8dbfcdbd4b72ba7dd
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-01 15:44:04 -0300

    trying to set the colors options for the Lojinha component

--- src/app/components/lojinha.tsx
+++ src/app/components/lojinha.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Tooltip } from "@material-tailwind/react";
 
 
 type ProdutosType = {
@@ -11,7 +12,7 @@ type ProdutosType = {
   imageURL2?: string | undefined;
   imageURL3?: string | undefined;
   insta: string;
-  info: string;
+  extraInfo: string;
   telefone: string;
   email: string;
   colorOptions?: boolean;
@@ -31,16 +32,16 @@ const produtos = [
     description: "petite description lorem ipsum dolor sit amet con el desc et",
     multiplePics: false,
     imageURL1: "/mockup.jpg",
-    // imageURL2: "",
-    // imageURL3: "",
+    imageURL2: null,
+    imageURL3: null,
     insta: "tramaink",
-    info: "Camiseta amarela - Free t-shirt Mockup",
+    extraInfo: "Camiseta amarela - Free t-shirt Mockup",
     telefone: "+55 11 99999-9999",
     email: "teste@hotmail.com",
     colorOptions: false,
-    // color1: "",
-    // color2: "",
-    // color3: "",
+    color1: null,
+    color2: null,
+    color3: null,
   },
   {
     id: 2,
@@ -50,15 +51,15 @@ const produtos = [
     multiplePics: true,
     imageURL1: "/mockup2.jpg",
     imageURL2: "/mockup3.jpg",
-    // imageURL3: "",
+    imageURL3: null,
     insta: "tramaink",
-    info: "lorem ipsum dolor sit amet con el desc",
+    extraInfo: "lorem ipsum dolor sit amet con el desc",
     telefone: "+55 11 99999-9999",
     email: "teste@hotmail.com",
     colorOptions: true,
-    color1: "#ffffff",
-    color2: "#000000",
-    // color3: "",
+    color1: "White",
+    color2: "Black",
+    color3: null,
   },
   {
     id: 3,
@@ -68,15 +69,15 @@ const produtos = [
     multiplePics: true,
     imageURL1: "/bone1.jpg",
     imageURL2: "/bone2.jpg",
-    // imageURL3: "",
+    imageURL3: null,
     insta: "atomicthrash",
-    info: "PRÉ VENDA ATÉ DIA 15 DE JANEIRO - O valor na pré venda é de 100R$ até a data limite. Somente 10 peças limitadas disponiveis, não vai rolar o mesmo modelo de novo",
+    extraInfo: "PRÉ VENDA ATÉ DIA 15 DE JANEIRO - O valor na pré venda é de 100R$ até a data limite. Somente 10 peças limitadas disponiveis, não vai rolar o mesmo modelo de novo",
     telefone: "+55 11 99999-9999",
     email: "teste@hotmail.com",
     colorOptions: false,
-    // color1: "",
-    // color2: "",
-    // color3: "",
+    color1: null,
+    color2: null,
+    color3: null,
   },
   {
     id: 4,
@@ -86,15 +87,15 @@ const produtos = [
     multiplePics: true,
     imageURL1: "/adesivos2.jpg",
     imageURL2: "/adesivos.jpg",
-    // imageURL3: "",
+    imageURL3: null,
     insta: "atomicthrash",
-    info: "Unidades limitadas - 6 stickers por embalagem",
+    extraInfo: "Unidades limitadas - 6 stickers por embalagem",
     telefone: "+55 11 99999-9999",
     email: "teste@hotmail.com",
     colorOptions: false,
-    // color1: "",
-    // color2: "",
-    // color3: "",
+    color1: null,
+    color2: null,
+    color3: null,
   },
 ]
 
@@ -112,34 +113,51 @@ export function Lojinha () {
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-items-center">
         {produtos.map((produto =>(
-        <div key={produto.id} className="m-2 border-2 border-green-500 w-fit">
-          <form className="border-2 border-yellow-500">
-            <Image 
-              className="rounded-md"
-              src={produto.imageURL1}
-              alt={produto.name} 
-              height={300}
-              width={300}/> 
-                <fieldset className="border-2 border-purple-500">
-                  <h3 className="font-Bungee font-medium text-md">{produto.name}</h3>
-                  <h3 className="font-Bungee text-sm text-text-secondary">R${produto.price}</h3>
-                    <legend className="sr-only">Choose a color</legend>
-                    <div className="forced-color-adjust-none border-2 border-orange-300">
-                      {produto.colorOptions === true ? 
+          <div key={produto.id} className="m-2 border-2 border-green-500 w-fit">
+            <form className="border-2 border-yellow-500">
+              <Image 
+                className="rounded-md"
+                src={produto.imageURL1}
+                alt={produto.name} 
+                height={300}
+                width={300}/> 
+              <div className="border-2 border-purple-500">
+                <h3 className="font-Bungee font-medium text-md">{produto.name}</h3>
+                <h3 className="font-Bungee text-sm text-text-secondary">R${produto.price}</h3>
+                  <div className="forced-color-adjust-none border-2 border-orange-300">
+
+                    {produto.color1  ? 
+                      <Tooltip content={produto.color1} className="bg-text-secondary font-Bungee text-sm">
                         <label className="m-1">
-                          <input className="" type="radio" name="color-choice" value="" />
-                          <span className="sr-only">{produto.colorOptions}</span>
-                          <span className="size-6 rounded-full border border-black border-opacity-10 bg-white"></span>
+                          <input className="size-4 rounded-full" type="radio" name={produto.color1} value={produto.color1} color={produto.color1} />
                         </label>
-                   
+                      </Tooltip>
+                    : '' }
+
+                    {produto.color2  ? 
+                      <Tooltip content={produto.color2} className="bg-text-secondary font-Bungee text-sm">
+                        <label className="m-1">
+                          <input  className="size-4 rounded-full" type="radio" name={produto.color2} value={produto.color2} color={produto.color2} />
+                          <span className="size-6 rounded-full"></span>
+                        </label>
+                        
+                      </Tooltip>
+                    : '' }
+
+                    {produto.color3  ?
+                      <Tooltip content={produto.color3} className="bg-text-secondary font-Bungee text-sm">
+                        <label className="m-1">
+                          <input className="size-4 rounded-full" type="radio" name={produto.color3} value={produto.color3} color={produto.color3} />
+                          <span className="size-6 rounded-full"></span>
+                        </label>
+                      </Tooltip>
+                    : '' }
                     
-                      
-                      : '' }
-                    </div>
-                  </fieldset>
-          </form>
-        </div>
-      )))}
+                  </div>
+              </div>
+            </form>
+          </div>
+        )))}
       </div>
     </div>
   )
---
+++ src/app/components/tooltip.tsx
@@ -0,0 +1,8 @@
+import { Tooltip, Button } from "@material-tailwind/react";
+ 
+export function TooltipDefault() {
+  return (
+    <Tooltip content="">
+    </Tooltip>
+  );
+}
